aaffe70f61005c117a35a045ca04229497295c7d
Lucas <lucas.barretto86@gmail.com>
2024-03-28 13:30:52 -0300

    Doing some image testing

--- SecondGame/Source/main.lua
+++ SecondGame/Source/main.lua
@@ -3,11 +3,11 @@ import "CoreLibs/graphics"
 local pd<const> = playdate
 local gfx<const> = pd.graphics
 
-local charImage = gfx.image.new("assets/sprites/chars/Bald")
+local charImage = gfx.image.new("assets/sprites/test/test-2")
 local char = gfx.sprite.new(charImage)
 
-char:moveTo(150, 50)
-char:setSize(32,32)
+char:moveTo(150, 100)
+char:setSize(64,64)
 char:add()
 
 function pd.update()
